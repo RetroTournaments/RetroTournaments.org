a65d75914481043f4e1d586d9f32562344c3f8a6
Matthew Deutsch <matthew@retrotournaments.org>
2025-05-25 10:16:40 -0600

    Add countdown to main page, clean up footer

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -1,23 +1,17 @@
 import { Link } from '@remix-run/react';
+import { useEffect } from 'react';
 import MailingListForm from './MailingListForm'
 
 function WelcomeTextHero() {
   return (
   <>
-    <div className="hero bg-base-100 min-h-48">
+    <div className="hero min-h-64">
       <div className="hero-content text-center">
-        <div className="max-w-3xl pl-8">
-          <h1 className="text-5xl font-bold">WELCOME TO RETROTOURNAMENTS.ORG</h1>
-        </div>
-      </div>
-    </div>
-    <div className="hero">
-      <div className="hero-content">
-        <div className="max-w-3xl pl-8">
-          <p className="py-6">
-            Welcome to the home of the <Link className="link link-primary" to="/about">Competitive Retro Gaming Association (CRGA)</Link>: A group of people working towards funner, and fairer, retro speedrunning competitions.
-            And also to the home of <Link className="link link-primary" to="/argos">Argos</Link>: the original open-source technology stack for simultaneous time attack tournaments.
-            We hope to see you at the next event!
+        <div className="max-w-2xl">
+          <h1 className="text-5xl font-bold">WELCOME TO RETRO TOURNAMENTS</h1>
+          <p className="pt-6">
+              Home of the <b><i>Simultaneous Time Attack</i></b>.
+              A fresh new competitive speedrunning format where multiple runners compete on identical setups combined onto the <b><i>same screen</i></b>.
           </p>
         </div>
       </div>
@@ -26,20 +20,91 @@ function WelcomeTextHero() {
   );
 }
 
-function SimulTimeAttackHero() {
+function YoutubeVideoHighlights() {
   return (
   <>
-  <div className="hero min-h-64">
-    <div className="hero-content flex-col sm:flex-row">
-      <div className="max-w-3xl pl-8">
-        <h4 className="text-3xl">Home of the</h4> 
-        <h1 className="text-5xl font-bold"><span className="text-primary"><i>Simultaneous Time Attack</i></span></h1>
-        <p className="py-6">
-            A fresh new competitive speedrunning format where multiple runners compete simultaneously on identical setups combined onto the <i>same screen</i>.
-            This format is more exciting, easier to follow, and way funner!
-        </p>
-        <Link to="/about"><button className="btn btn-primary">Learn more</button></Link>
+    <div className="flex justify-center">
+      <div className="max-w-3xl w-full">
+        <h1 className="text-2xl font-bold">Check out these highlights from a past event</h1>
+        <div style={{"position": "relative", "width":"100%", "height":"0", "padding-bottom":"56.25%"}}>
+          <iframe src="https://www.youtube.com/embed/3WRWz7XG3Os?si=eZmOm2_juYXavVcq" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen style={{"position":"absolute", "top":"0", "left":"0", "width":"100%", "height":"100%"}}></iframe>
+        </div>
       </div>
+    </div>
+  </>
+  );
+}
+
+function CountdownToSOCAL() {
+  useEffect(() => {
+      const interval = setInterval(() => {
+        let now = new Date();
+        let socal_date = new Date("2025-06-06T13:00:00");
+
+        let seconds = Math.floor((socal_date - now) / 1000);
+        let minutes = Math.floor(seconds / 60);
+        let hours = Math.floor(minutes / 60);
+        let days = Math.floor (hours / 24);
+
+        hours = hours - (days * 24);
+        minutes = minutes - (days * 24 * 60 + hours * 60);
+        seconds = seconds % 60;
+
+        let span = document.getElementById('countdown-days');
+        span.textContent = `${days}`;
+        span.setAttribute('style', `--value: ${days}`);
+
+        span = document.getElementById('countdown-hours');
+        span.textContent = `${hours}`;
+        span.setAttribute('style', `--value: ${hours}`);
+
+        span = document.getElementById('countdown-minutes');
+        span.textContent = `${minutes}`;
+        span.setAttribute('style', `--value: ${minutes}`);
+
+        span = document.getElementById('countdown-seconds');
+        span.textContent = `${seconds}`;
+        span.setAttribute('style', `--value: ${seconds}`);
+      }, 1000);
+      return () => clearInterval(interval);
+  }, []);
+
+  return (
+  <>
+    <div className="flex justify-center pb-4">
+      <div className="max-w-2xl w-full">
+        <div className="divider"></div>
+        <div className="flex justify-center">
+          <h1 className="text-5xl font-bold">Countdown to <Link to="/events" className="link link-hover text-accent">SOCAL 2025!!</Link></h1>
+        </div>
+
+        <div className="flex gap-5 justify-center pt-4">
+          <div>
+            <span className="countdown font-mono text-4xl">
+                <span id="countdown-days" style={{"--value":0}}>0</span>
+            </span>
+            &nbsp;days
+          </div>
+          <div>
+            <span className="countdown font-mono text-4xl">
+                <span id="countdown-hours" style={{"--value":0}}>0</span>
+            </span>
+            &nbsp;hours
+          </div>
+          <div>
+            <span className="countdown font-mono text-4xl">
+                <span id="countdown-minutes" style={{"--value":0}}>0</span>
+            </span>
+            &nbsp;minutes
+          </div>
+          <div>
+            <span className="countdown font-mono text-4xl">
+                <span id="countdown-seconds" style={{"--value":0}}>0</span>
+            </span>
+            &nbsp;seconds
+          </div>
+        </div>
+        <div className="divider"></div>
     </div>
   </div>
   </>
@@ -49,8 +114,9 @@ function SimulTimeAttackHero() {
 function LandingPage() {
   return (
     <>
-    <WelcomeTextHero />
-    <SimulTimeAttackHero />
+      <WelcomeTextHero/ >
+      <CountdownToSOCAL/ >
+      <YoutubeVideoHighlights/ >
     </>
   );
 }
